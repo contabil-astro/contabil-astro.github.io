cc8324d268311d367eefe5b0e2917e5f874b2f0c
Roberto Griel Filho <roberto@griel.com.br>
2020-10-29 21:49:33 -0300

    Header and footer mixin; Menu developing

---
+++ system/javascript/openMenu.js
@@ -0,0 +1,4 @@
+document.querySelector("#menu-handler").addEventListener("click", function(){
+    document.querySelector("nav").classList.toggle("open");
+    document.querySelector("#menu-handler").classList.toggle('open');
+});
